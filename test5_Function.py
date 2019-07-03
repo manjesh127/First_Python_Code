578a6bcfe6d54b52f2c6281d88951e17958a2943
manjesh singh <majeshsingh127@gmail.com>
2019-07-03 13:14:37 +0530

    update

--- test5_Function.py
+++ test5_Function.py
@@ -1,11 +1,19 @@
 # ==================>>>  function in python define by keyword def (reserver)  <<<====================
-def greet_user():  # == :  define a block of code in function
-    print("hii manjesh you are inside function")
+# def greet_user():  # == :  define a block of code in function
+#     print("hii manjesh you are inside function")
+#     print("enjoy funtion")
+
+
+# print("start")
+# greet_user()
+# print("finish")
+
+# ===================>>> parameter <<=====================
+def greet_user(name):
+    print(f"hii {name} you are inside function")
     print("enjoy funtion")
 
 
 print("start")
-greet_user()
+greet_user("manjesh")
 print("finish")
-
-# ===================>>> parameter <<=====================
